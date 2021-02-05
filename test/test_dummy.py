6458acef7cc5d19db32c60a68513709e05d2cd65
cgloeckner <cgloeckner@freenet.de>
2021-02-05 08:13:29 +0100

    Vorbereitungen für Unittesting

---
+++ test/test_dummy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest, webtest
+
+
+class ExampleTest(unittest.TestCase):
+
+	def setUpUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_dummy(self):
+		pass
