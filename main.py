e9967d00c8591da5916b8a77b8307253d2e72074
cgloeckner <cgloeckner@freenet.de>
2025-02-13 16:14:00 +0100

    argv --port=8080

--- main.py
+++ main.py
@@ -10,44 +10,56 @@ from db.orm import db, db_session
 from pony import orm
 from bottle import *
 import locale
+import sys
 
 
 __author__ = "Christian Glöckner"
 
 
-# determine school year and load suitable database
+def main():
+    # determine school year and load suitable database
 
-s = utils.Settings()
-try:
-    with open('settings.ini') as h:
-        s.load_from(h)
-except FileNotFoundError:
-    # keep default values
-    pass
+    s = utils.Settings()
+    try:
+        with open('settings.ini') as h:
+            s.load_from(h)
+    except FileNotFoundError:
+        # keep default values
+        pass
 
-debug = True
+    debug = True
 
-db.bind(
-    'sqlite',
-    'data%s.db' %
-    s.data['general']['school_year'],
-    create_db=True)
-db.generate_mapping(create_tables=True)
+    db.bind(
+        'sqlite',
+        'data%s.db' %
+        s.data['general']['school_year'],
+        create_db=True)
+    db.generate_mapping(create_tables=True)
 
-app = default_app()
-app.catchall = not debug
-app.install(db_session)
-
-
-@get('/static/<fname>')
-def static_files(fname):
-    return static_file(fname, root='./static')
+    app = default_app()
+    app.catchall = not debug
+    app.install(db_session)
 
 
-@get('/')
-@view('home')
-def landingpage():
-    return dict()
+    @get('/static/<fname>')
+    def static_files(fname):
+        return static_file(fname, root='./static')
 
 
-run(host='localhost', reloader=True, debug=debug, port=8000)
+    @get('/')
+    @view('home')
+    def landingpage():
+        return dict()
+
+    port = 8000
+    for val in sys.argv:
+        if not val.startswith('--port='):
+            continue
+
+        port = int(val.split('--port=')[1])
+
+    run(host='localhost', reloader=True, debug=debug, port=port)
+
+
+if __name__ == '__main__':
+    main()
